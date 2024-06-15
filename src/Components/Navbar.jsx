f3ec632779578f4913a0d563ca0a253eec49dec6
sjilani443 <143477393+sjilani443@users.noreply.github.com>
2024-06-15 18:03:44 +0530

    small

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <nav className='h-20 w-full flex  sm:justify-between items-center pt-4 sm:text-2xl sm:px-28 mb-10 font-space'>
        <Link  to='/'> <img src={portlogo} alt="" className='h-20' /></Link>
-      <div className='flex ml-14 gap-4 text-md font-bold'>
+      <div className='flex lg:ml-14 gap-4 text-md font-bold sm:gap-10 sm:font-normal'>
       <Link to='/'><button>Home</button></Link>
       <Link to='/projects'><button>Projects</button></Link>
       <Link to='/contact'><button>Contact</button></Link>
